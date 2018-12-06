702473f94c1d5e9639cda9fdd29bdd992f83bb36
seungjin.han <hanblues@gmail.com>
2018-12-06 13:10:05 +0900

    Modify news crawling

--- naver_finance_news/news_crawing.py
+++ naver_finance_news/news_crawing.py
@@ -1,6 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import datetime
+from pymongo import MongoClient
+from time import sleep
 '''
 
 url="https://finance.naver.com/news/news_list.nhn";
@@ -46,6 +48,10 @@ day_index = 0;
 first_title = ''
 first_check = 0
 
+client = MongoClient('localhost', 27017)
+db = client['news']
+collection = db['naver_finance_news	']
+
 while True:
 
     is_break = 0
@@ -53,7 +59,9 @@ while True:
     
     now = now + datetime.timedelta(days=day_index)
     nowDate = now.strftime('%Y%m%d');
-        
+
+    sleep(1)   # 1초간 딜레이 시킴
+         
     url = 'https://finance.naver.com/news/news_list.nhn?mode=RANK&date='+str(nowDate)+'&page='+str(index)+""
     print(url)
     req = requests.get(url)
@@ -64,7 +72,6 @@ while True:
     soup = BeautifulSoup(html, 'html.parser')
     data = soup.find_all('ul', class_="simpleNewsList")
     
-    print( len(data))
     if len(data) == 1 :
         
         day_index = -1
@@ -81,20 +88,45 @@ while True:
                 day_index = -1
                 index = 1
                 break;
-                
+
+            is_input = False;    
             for j in range(len(news)):
                 
                 if first_check == 0:
                 
-                    print("1.",first_title)
-                    print("2.",news[j].a['title'])
                     if first_title == news[j].a['title']:
-                        print('1')
                         first_title = ''
                         first_check = 0
                         is_break = 1
+                        is_input = False
                         break;
                     else:
-                        print('2')
                         first_title = news[j].a['title']
                         first_check = 1
+                        is_input = True
+                else:
+                    is_input = True
+
+                #DB 에 저장한다.
+                if is_input == True:
+                    link = news[j].a
+                    str_link = str(link).replace('<a href="' , '<a href="https://finance.naver.com')
+                    str_link = str_link.replace('&amp;' , '&')
+                    
+                    title = news[j].a['title']
+                    press = news[j].find_all('span' , class_='press')[0].contents[0] 
+                    wdate = news[j].find_all('span' , class_='wdate')[0].contents[0]
+
+                    findDataCount = collection.count_documents({'title':title})
+                    if findDataCount == 0:
+                        in_data = {
+                            'title':title,
+                            'link':str_link,
+                            'press':press,
+                            'wdate':wdate
+                        }                        
+                        collection.insert( in_data)
+                        print( title)
+                        
+                    
+                    
